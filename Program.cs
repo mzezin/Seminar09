e9681bb116e663397f1b5174d8cad1791c186a35
Maksim Zezin <mzezin@gmail.com>
2022-09-02 21:38:05 +0300

    Error processing rework

--- Program.cs
+++ Program.cs
@@ -10,21 +10,22 @@ void PrintArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i == array.Length - 1) Console.Write(array[i]);
-        else Console.Write(array[i] + ", ");
+        if (i == array.Length - 1)
+            Console.Write(array[i]);
+        else
+            Console.Write(array[i] + ", ");
     }
     Console.WriteLine("]");
 }
 
-int[] processInput() //на вход получает массив данных от пользователя через запятую и на выходе выдаёт массив
+int[] processInput(string input) //на вход получает массив данных от пользователя через запятую и на выходе выдаёт массив
 {
-    string input = Console.ReadLine();
     string[] stringArray = input.Split(",");
     int[] result = new int[stringArray.Length];
     for (int i = 0; i < stringArray.Length; i++)
     {
         if (!int.TryParse(stringArray[i], out int number))
-            throw new Exception("Ошибка ввода");
+            return new int[0] { };
         result[i] = number;
     }
     return result;
@@ -53,6 +54,18 @@ int[] SelectArray(int[] array)
     return result;
 }
 
-int[] array = processInput();
+
+int[] array = new int[0];
+bool correctInput = false;
+while (!correctInput)
+{
+    string inputString = Console.ReadLine();
+    array = processInput(inputString);
+    if (array.Length > 0)
+        correctInput = true;
+    else
+        Console.WriteLine("Неправильый ввод");
+}
+
 int[] evenArray = SelectArray(array);
 PrintArray(evenArray);
