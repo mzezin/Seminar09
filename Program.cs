342d3a97c8adc9ee63bb37f38dccaabaeede85b8
Said Yarullin <79087625+Ka3aner@users.noreply.github.com>
2022-09-02 20:43:13 +0300

    PrintArray Function Added

--- Program.cs
+++ Program.cs
@@ -3,3 +3,13 @@
 
 // пример:
 // 1,2,3,4,5,6,7,8 => 2 4 6 8
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write(array[i] + "]");
+        else Console.Write(array[i] + ", ");
+    }
+}
